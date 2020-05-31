1c4c0eecc4837cf9ff34912c2bd41a3aca3397af
vanrest2016 <56249174+vanrest2016@users.noreply.github.com>
2020-05-31 10:19:41 +0300

    Update csv.c

--- csv.c
+++ csv.c
@@ -1,38 +1,172 @@
-#pragma once
-#include <stdlib.h>
 #include <stdio.h>
+#include <conio.h>
+#include <string.h>
 
-char* fget_string(FILE* file_books_csv) {
-    char* s = (char*) calloc(1024, sizeof(char));
-    int pos = 0;
-    for(char c = getc(file_books_csv); c != '\n' && c != EOF; c = getc(file_books_csv)) {
-        s[pos++] = c;
+/* Ñòðóêòóðà äëÿ õðàíåíèÿ èíôîðìàöèè ïðî îäíîãî ÷åëîâåêà */
+typedef struct tagITEM
+{
+  char Surname[20], Name[20];
+  int Age;
+} ITEM;
+
+/* Ìàññèâ ìàêñèìóì íà 20 ÷åëîâåê */
+ITEM Men[20];
+
+/* Êîëè÷åñòâî çàíÿòûõ ýëåìåíòîâ â ìàññèâå */
+int Number;
+
+/* Ôóíêöèÿ äëÿ âûâîäà âñåõ ýëåèåíòîâ */
+void Print( void )
+{
+  int i;
+
+  printf(" N  Surname             Name                 Age\n");
+  for (i = 0; i < Number; i ++)
+    printf("%2i. %-20s %-20s %3i\n", i + 1, Men[i].Surname,
+           Men[i].Name, Men[i].Age);
+} 
+
+/* Ôóíêöèÿ äëÿ äîáàâëåíèÿ ýëåìåíòà */
+void Add( void )
+{
+  if (Number == 19)
+  {
+    fprintf(stderr, "Element don't add'\n");
+    return;
+  }
+
+  printf("Surname > ");
+  scanf("%s", Men[Number].Surname);
+  printf("Name > ");
+  scanf("%s", Men[Number].Name);
+  printf("Age> ");
+  scanf("%i", &Men[Number].Age);
+
+  Number ++;
+} 
+
+/* Ôóíêöèÿ äëÿ óäàëåíèÿ ýëåìåíòà */
+void Del( void )
+{
+  int i;
+
+  Print();
+  printf("Enter number of element > ");
+  scanf("%i", &i);
+  if (i < 1 || i > Number)
+  {
+    fprintf(stderr, "Element %i don't exists'\n", i);
+    return;
+  }
+
+  for (i --; i < Number - 1; i ++)
+    Men[i] = Men[i + 1];
+
+  Number --;
+} 
+
+/* Ôóíêöèÿ äëÿ ñîõðàíåíèÿ ìàññèâà â ôàéëå */
+void Save( void )
+{
+  FILE *F;
+  int i;
+
+  if ((F = fopen("task1.csv", "wt")) == NULL)
+  {
+    fprintf(stderr, "File opened error 'task1.csv'\n");
+    return;
+  }
+
+  fprintf(F, "%i\n", Number);
+  for (i = 0; i < Number; i ++)
+    fprintf(F, "%s\n%s\n%i\n", Men[i].Surname, Men[i].Name, Men[i].Age);
+
+  fclose(F);
+} 
+
+/* Ôóíêöèÿ äëÿ ÷òåíèÿ ìàññèâà èç ôàéëà */
+void Load( void )
+{
+  FILE *F;
+  int i;
+
+  if ((F = fopen("task1.csv", "rt")) == NULL)
+  {
+    fprintf(stderr, "File opened Error\n");
+    return;
+  }
+
+  fscanf(F, "%i", &Number);
+  for (i = 0; i < Number; i ++)
+    fscanf(F, "%s%s%i", Men[i].Surname, Men[i].Name, &Men[i].Age);
+
+  fclose(F);
+} 
+
+/* Ôóíêöèÿ äëÿ óïîðÿäî÷èâàíèÿ ìàññèâà ïî ôàìèëèè */
+void Sort( void )
+{
+  int i, j;
+  ITEM Temp;
+
+  for (j = Number - 1; j > 0; j --)
+    for (i = 0; i < j; i ++)
+      if (strcmp(Men[i].Surname, Men[i + 1].Surname) > 0)
+      {
+        Temp = Men[i];
+        Men[i] = Men[i + 1];
+        Men[i + 1] = Temp;
+      }
+} 
+
+/* Âûâîä ìåíþ è ÷òåíèå íîìåðà âûáðàííîãî ïóíêòà */
+int Menu( void )
+{
+  int c = 0;
+
+  while ((c < '0' || c > '6') && c != 27)
+  {
+    printf("0 : Exit\n"
+           "1 : Add\n"
+           "2 : Save\n"
+           "3 : Load\n"
+           "4 : Enter\n"
+           "5 : Sort\n"
+           "6 : Delete\n"
+           ">");
+    c = getch();
+    printf("%c\n", c);
+  }
+  return c;
+} 
+
+/* Îñíîâíàÿ ôóíêöèÿ */
+int main()
+{
+  int Selection;
+
+  Number = 0;
+  while ((Selection = Menu()) != '0' && Selection != 27)
+    switch (Selection)
+    {
+    case '1':
+      Add();
+      break;
+    case '2':
+      Save();
+      break;
+    case '3':
+      Load();
+      break;
+    case '4':
+      Print();
+      break;
+    case '5':
+      Sort();
+      break;
+    case '6':
+      Del();
+      break;
     }
-    s[pos++] = '\0';
-    return s;
 }
 
-char* parse_csv(char* s) {
-    char* t = (char *) calloc(255, sizeof(char));
-    if (s[0] == '"') { // Р•СЃР»Рё Сѓ РЅР°СЃ РІ РєР°РІС‹С‡РєР°С… РІСЃРµ
-        if(strstr(s, ",")==NULL) {
-            strncpy(t,s+1, strstr(s+1, "\"")-s-1);
-            t[strstr(s+1, "\"")-s-1] = '\0';
-            strncpy(s, "", 1);
-            return t;
-        }
-        strncpy(t, s+1, strstr(s, "\",")-s-1); // РўРѕ РёС‰РµРј РґРѕ ", - РєРѕРЅРµС† С‚РѕРіРѕ, С‡С‚Рѕ РІС‹СЂР·Р°РµРј
-        t[strstr(s, "\",")-s-1] = '\0';
-        strcpy(s, strstr(s, "\",")+2);
-        return t;   
-    } else if(strstr(s, ",")!=NULL) { // РРЅР°С‡Рµ РµСЃР»Рё РµС‰Рµ РµСЃС‚СЊ Р·Р°РїСЏС‚Р°СЏ
-        strncpy(t, s, strstr(s, ",")-s); //РІС‹СЂРµР·Р°РµРј РґРѕ РЅРµРµ
-        t[strstr(s, ",")-s] = '\0';
-        strcpy(s, strstr(s, ",")+1);
-        return t;     
-    }
-    strncpy(t, s, strlen(s));
-    t[strlen(s)] = '\0';
-    strncpy(s, "", 1);
-    return t;
-}
